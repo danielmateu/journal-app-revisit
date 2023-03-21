c3c7fddd7e52b59edae9c5c822ca0d0ebd698a09
Daniel Mateu <danielmateu86@gmail.com>
2023-03-21 17:31:18 +0100

    Diseño del LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,55 @@
+import { Grid, TextField, Typography } from "@mui/material"
 
 
 export const LoginPage = () => {
     return (
-        <>
-            <h1>LoginPage</h1>
-        </>
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+        >
+            <Grid
+                item
+                xs={3}
+                className='box-shadow'
+                sx={{ backgroundColor: 'background.paper', padding: 4, borderRadius: 2 }}
+            >
+                <Typography variant='h4' component='h1' align='center' gutterBottom>Login</Typography>
+
+                <form action="">
+                    <Grid
+                        container
+                        spacing={2}
+                        // direction="column"
+                        // alignItems="center"
+                        // justifyContent="center"
+                    >
+                        <Grid item xs={12}>
+                            <TextField
+                                label='Correo'
+                                type={'email'}
+                                fullWidth
+                                required
+                                placeholder="correo@gmail.com"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label='Contraseña'
+                                type={'password'}
+                                fullWidth
+                                required
+                                placeholder="********"
+                            />
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+
+        </Grid>
     )
 }
 
