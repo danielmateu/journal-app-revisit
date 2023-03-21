96ed669e6eb0366727eb4b5c6687e405d32b3575
Daniel Mateu <danielmateu86@gmail.com>
2023-03-21 17:03:42 +0100

    Estructura de la app + instalación de mui + icons ui

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
     return (
         <>
-            <h1>Hola mundo</h1>
+            <AppRouter />
         </>
     )
 }
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JournalPage = () => {
+    return (
+        <>
+            <h1>JournalPage</h1>
+        </>
+    )
+}
+
+export default JournalPage
