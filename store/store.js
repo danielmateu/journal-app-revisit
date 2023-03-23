89878da49dd04316a447678ef11f67dceb7587f0
Daniel Mateu <danielmateu86@gmail.com>
2023-03-23 10:45:33 +0100

    configuracion de authSlice

--- store/store.js
+++ store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authSlice } from './auth/authSlice'
 
 export const store = configureStore({
     reducer: {
         // Add your reducers here
+        auth: authSlice.reducer,
     },
 })
