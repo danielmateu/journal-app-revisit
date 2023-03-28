72e195ccc8d9c66a90a34e370313f0ebd6e51366
Daniel Mateu <danielmateu86@gmail.com>
2023-03-28 15:00:15 +0200

    Pruebas sobre authSlice

--- test/store/auth/authSlice.test.js
+++ test/store/auth/authSlice.test.js
@@ -1,12 +1,40 @@
-import { authSlice } from "../../../store/auth/authSlice"
-import { initialState } from "../../fixtures/authFixtures";
+import { authSlice, login, logout } from "../../../store/auth/authSlice"
+import { authenticatedState, demoUser, initialState, notAuthenticatedState } from "../../fixtures/authFixtures";
 
 describe('Tests sobre authSlice', () => {
     test('Debe regresar el estado inicial y llamarse "auth"', () => {
-        
+
         const state = authSlice.reducer(initialState, {})
-        expect (authSlice.name).toBe('auth');
+        expect(authSlice.name).toBe('auth');
         expect(state).toEqual(initialState);
 
+    })
+
+    test('Debe de realizar la autenticación', () => {
+
+        // console.log(login(demoUser));
+
+        const state = authSlice.reducer(initialState, login(demoUser))
+        // console.log(state);
+        expect(state).toEqual(authenticatedState)
+    })
+
+    test('Debe de realizar el logout', () => {
+
+        const state = authSlice.reducer(authenticatedState, logout())
+        console.log(state);
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: undefined
+        })
+    })
+
+    test('Debe realizar el logout y mostrar un mensaje de error', () => {
+        const state = authSlice.reducer(authenticatedState, logout({ errorMessage: 'Error en el servidor' }))
+        expect(state).toEqual({ ...notAuthenticatedState, errorMessage: 'Error en el servidor' })
     })
 })
