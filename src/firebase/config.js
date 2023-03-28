0fa62f7265d63e8092a26e7134cda8638aeb8ee8
Daniel Mateu <danielmateu86@gmail.com>
2023-03-28 20:02:00 +0200

    COnfiguracion en prod

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,25 +6,25 @@ import { getFirestore } from "firebase/firestore/lite";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCw3oNORHIWRNNxG_U0OAbT8rawQFD7R8E",
-//     authDomain: "react-cursos-8bd77.firebaseapp.com",
-//     projectId: "react-cursos-8bd77",
-//     storageBucket: "react-cursos-8bd77.appspot.com",
-//     messagingSenderId: "1067189630477",
-//     appId: "1:1067189630477:web:c0d65c862373909cb54744"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyCw3oNORHIWRNNxG_U0OAbT8rawQFD7R8E",
+    authDomain: "react-cursos-8bd77.firebaseapp.com",
+    projectId: "react-cursos-8bd77",
+    storageBucket: "react-cursos-8bd77.appspot.com",
+    messagingSenderId: "1067189630477",
+    appId: "1:1067189630477:web:c0d65c862373909cb54744"
+};
 
 //testing
-const firebaseConfig = {
-    apiKey: "AIzaSyDlJeyZdvRd4Iask8303pqrBytvXn2Ljuc",
-    authDomain: "cloud-firestore-ccc71.firebaseapp.com",
-    projectId: "cloud-firestore-ccc71",
-    storageBucket: "cloud-firestore-ccc71.appspot.com",
-    messagingSenderId: "31820881864",
-    appId: "1:31820881864:web:42ffc284260b35fee62afd",
-    measurementId: "G-0RGCPSNVMG"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDlJeyZdvRd4Iask8303pqrBytvXn2Ljuc",
+//     authDomain: "cloud-firestore-ccc71.firebaseapp.com",
+//     projectId: "cloud-firestore-ccc71",
+//     storageBucket: "cloud-firestore-ccc71.appspot.com",
+//     messagingSenderId: "31820881864",
+//     appId: "1:31820881864:web:42ffc284260b35fee62afd",
+//     measurementId: "G-0RGCPSNVMG"
+//   };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
